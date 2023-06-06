93e3f570ebdc17dfdc3d13fdedb4b8462cee7c1f
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-06-06 09:53:55 -0400

    Assignment completed

---
+++ FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Peter Vasilopoulos");
+        System.out.println("I am 25 years old");
+        System.out.println("My hometown is Arlington, VA");
+    }
+}
